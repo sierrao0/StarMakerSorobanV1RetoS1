779b400e8929b9238ef35024e8974f978d87f73c
Sergio Martinez <sergiotechx@yahoo.com>
2025-06-13 21:25:01 -0500

    enviar todo

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![no_std]
+
+
+use soroban_sdk::{contract, contractimpl,  Env, symbol_short, Symbol};
+const RESULTADO: Symbol = symbol_short!("RESULTADO");
+#[contract]
+pub struct Contract;
+
+
+#[contractimpl]
+impl Contract {
+    
+    pub fn sumar(env: Env, a:i128, b:i128) -> i128 {
+      //Implementar función que sume dos números
+    }
+
+    pub fn resultado_anterior(env: Env) -> i128 {
+           //Implementar función que retorne el valor anterior
+    }
+}
+
+mod test;
